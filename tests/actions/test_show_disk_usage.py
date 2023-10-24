d32b7ea7db191072631712b5d8ca3c2ad45627d2
Lívio Lopes <antonio.lvio760@gmail.com>
2023-10-24 19:31:08 -0300

    refactor(test/show_disk_usage): implemented mock _get_printable_file_path

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,4 +1,5 @@
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+from unittest.mock import Mock, patch
 
 
 def test_show_disk_usage_context_empty(capsys):
@@ -19,15 +20,31 @@ def test_show_disk_usage_context_all_files(capsys, tmp_path):
     tmp_file_2.write_text(CONTENT * 2)
     tmp_file_3 = tmp / "file3.txt"
     tmp_file_3.write_text(CONTENT * 3)
-    expected_output = (
-        "'/tmp/pytest-of-liviolopes/p...age_context_a0/tests/file3.txt':"
-        "        12 (50%)\n'/tmp/pytest-of-liviolopes/p...age_context_a0/"
-        "tests/file2.txt':        8 (33%)\n'/tmp/pytest-of-liviolopes/p..."
-        "age_context_a0/tests/file1.txt':        4 (16%)\nTotal size: 24\n"
-    )
+
     context = {
-        "all_files": [f"{tmp_file_1}", f"{tmp_file_2}", f"{tmp_file_3}"]
+        "all_files": [
+            f"{tmp_file_1}",
+            f"{tmp_file_2}",
+            f"{tmp_file_3}",
+        ]
     }
-    show_disk_usage(context)
-    captured = capsys.readouterr()
-    assert captured.out == expected_output
+
+    mock_get_printable_file_path = Mock(
+        return_value=("/tmp/pytest-of-liviolopes/p...age_context_a0")
+    )
+    expected_output = (
+        "'/tmp/pytest-of-liviolopes/p..."
+        "age_context_a0':                        12 (50%)\n'/tmp/"
+        "pytest-of-liviolopes/p..."
+        "age_context_a0':                        8 (33%)\n'/tmp/"
+        "pytest-of-liviolopes/p..."
+        "age_context_a0':                        4 (16%)\nTotal "
+        "size: 24\n"
+    )
+    with patch(
+        "pro_filer.actions.main_actions._get_printable_file_path",
+        mock_get_printable_file_path,
+    ):
+        show_disk_usage(context)
+        captured = capsys.readouterr()
+        assert captured.out == expected_output
